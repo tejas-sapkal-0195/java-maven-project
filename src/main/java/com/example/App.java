37971f300aead6f16dd3dbb7cb537da33d9ceef8
Atul Kamble <atul_kamble@hotmail.com>
2026-03-10 07:54:51 +0530

    update

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World from Amazon Linux Maven Project!");
+
+    }
+
+}
